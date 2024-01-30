d7f93230813bdd48020748fc51aa1cff26d2e1c5
XAntonioLuna <lunagantonio@gmail.com>
2024-01-30 15:24:40 -0800

    More changes

--- README.md
+++ README.md
@@ -108,8 +108,3 @@ To learn more about Gatsby themes specifically, I recommend checking out the [th
 
 - **To dive straight into code samples, head [to Gatsby's documentation](https://www.gatsbyjs.com/docs/).** In particular, check out the _How-to Guides_ and _Reference_ items in the primary navigation.
 
-## 🌟 Supporting me
-
-Thanks for using this project! I'm always interested in seeing what people do with my projects, so don't hesitate to tag me on [Twitter](https://twitter.com/lekoarts_de) and share the project with me.
-
-Please star this project, share it on Social Media or consider supporting me on [GitHub Sponsors](https://github.com/sponsors/LekoArts)!
--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,21 +1,18 @@
 import type { GatsbyConfig, PluginRef } from "gatsby"
 import "dotenv/config"
 
-const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
-
 const config: GatsbyConfig = {
   siteMetadata: {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitleAlt: `Antonio Luna`,
+    siteHeadline: `Antonio Luna - Portfolio`,
+    siteUrl: `https://xantonioluna.github.io/`,
+    siteDescription: `Portfolio site for Antonio Luna. Built with Gatsby with a theme by LekoArts.`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    author: `@XAntonioLuna`,
   },
   trailingSlash: `always`,
   plugins: [
@@ -27,9 +24,9 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Antonio Luna - Portfolio`,
+        short_name: `Antonio Luna`,
+        description: `Portfolio site for Antonio Luna. Built with Gatsby with a theme by LekoArts.`,
         start_url: `/`,
         background_color: `#141821`,
         // This will impact how browsers show your PWA/website
@@ -48,15 +45,6 @@ const config: GatsbyConfig = {
             type: `image/png`,
           },
         ],
-      },
-    },
-    // You can remove this plugin if you don't need it
-    shouldAnalyseBundle && {
-      resolve: `gatsby-plugin-webpack-statoscope`,
-      options: {
-        saveReportTo: `${__dirname}/public/.statoscope/_bundle.html`,
-        saveStatsTo: `${__dirname}/public/.statoscope/_stats.json`,
-        open: false,
       },
     },
   ].filter(Boolean) as Array<PluginRef>,
